ddc1cbe77db0dd2f82e70b4ec079ecd9110803e9
Alexander Wong <wong.a@etoncollege.org.uk>
2024-11-15 17:07:53 +0000

    Initial Commit

---
+++ StandardAlgorithms2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StandardAlgorithms2
+//
+//  Created by Wong, Alex (JD) on 15/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StandardAlgorithms2/StandardAlgorithms2App.swift
@@ -0,0 +1,17 @@
+//
+//  StandardAlgorithms2App.swift
+//  StandardAlgorithms2
+//
+//  Created by Wong, Alex (JD) on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct StandardAlgorithms2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
